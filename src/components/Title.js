614ec64ee58a4e4edd8b7aef6415e69ed93b2181
Kyle Lynds <kylelynds@Kyles-MBP.lan>
2020-10-08 14:48:58 -0700

    styles-added

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,13 +1,18 @@
 import React from "react"
 import Date from "./Date"
+import styled from 'styled-components'
+
+const StyledHeader = styled.div`
+    color: red;
+`
 
 export default function Title(props) {
     const {titleData, dateData} = props
 
     return (
-        <div classname = 'title-div'>
+        <StyledHeader>
             <h1>{titleData}</h1>
             <h3><Date nasaDateData={dateData}/></h3>
-        </div>
+       </StyledHeader>
     )
 }
